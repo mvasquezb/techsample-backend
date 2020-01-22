0ad6beed4a516223766c5abc98a7ff76e6e1b5c0
Martin Vasquez <mvasquez@karaokesmart.co>
2020-01-21 19:56:51 -0500

    WIP user login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,33 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Log the user in
+     * 
+     * @param Requestt $request
+     */
+    public function login(Request $request) {
+        $email = $request->email;
+        $password = $request->password;
+
+        return bcrypt($password);
+
+        $user = User::where([
+            ['email', '=', $email],
+            ['password', '=', Hash::make($password)],
+        ])->get();
+        
+        if ($user->isEmpty()) {
+            return response(
+                [
+                    "message" => "Invalid user credentials",
+                ],
+                404
+            );
+        }
+
+        return $user;
     }
 }
