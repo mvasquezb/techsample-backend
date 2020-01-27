91f0d3f20b0a9cda335b605bbceaa4846552c9cc
Martin Vasquez <mvasquez@karaokesmart.co>
2020-01-27 14:17:09 -0500

    Add user login tests

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,12 +2,15 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Str;
 
 
 class UserTest extends TestCase
 {
+    use WithFaker;
+
     /**
      * Test successful user registration.
      *
@@ -16,6 +19,7 @@ class UserTest extends TestCase
     public function testRegisterSuccess()
     {
         $user = factory(\App\User::class)->make();
+        $email = $user->email;
         $response = $this->json('POST', '/api/register', $user->attributesToArray() + [
             'password' => '1234',
             'password_confirmation' => '1234',
@@ -26,6 +30,10 @@ class UserTest extends TestCase
             ->assertJsonStructure([
                 'token'
             ]);
+
+        $this->assertDatabaseHas('users', [
+            'email' => $email
+        ]);
     }
 
     /**
@@ -33,7 +41,7 @@ class UserTest extends TestCase
      *
      * @return void
      */
-    public function testRegisterErrorUserExists()
+    public function testRegisterFailUserExists()
     {
         $existingUser = factory(\App\User::class)->create([
             'gamertag' => 'test'
@@ -48,6 +56,47 @@ class UserTest extends TestCase
 
         $response
             ->assertStatus(400)
+            ->assertJsonStructure([
+                'errors'
+            ]);
+    }
+
+    /**
+     * Test successful login
+     * 
+     * @return void
+     */
+    public function testLoginSuccess()
+    {
+        $user = factory(\App\User::class)->create([
+            'password' => bcrypt('1234'),
+        ]);
+        $response = $this->json('POST', '/api/login', [
+            'email' => $user->email,
+            'password' => '1234'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'token'
+            ]);
+    }
+
+    /**
+     * Test unsuccessful login
+     * 
+     * @return void
+     */
+    public function testLoginFail()
+    {
+        $response = $this->json('POST', '/api/login', [
+            'email' => $this->faker->email,
+            'password' => $this->faker->password
+        ]);
+
+        $response
+            ->assertStatus(401)
             ->assertJsonStructure([
                 'errors'
             ]);
